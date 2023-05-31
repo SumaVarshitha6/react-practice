f6b516e8f25e3d09dbfa36b843a28947364067de
SumaVarshitha6 <Sumavarshitha216@gmail.com>
2023-05-31 13:30:20 +0530

    useeffect

--- mypractice/src/greet.jsx
+++ mypractice/src/greet.jsx
@@ -1,4 +1,11 @@
+import React from 'react';
 function First(props){
+    React.useEffect(()=>{
+        console.log("First Comp useEffect with empty deps")
+      },[])
+    React.useEffect(()=>{
+        console.log("First Comp useEffect with no deps")
+      })
     return (
         <div className='border border-1 border-dark p-3 m-2'>
         <b>First...</b><br></br>
