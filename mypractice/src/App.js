3869443fe3adc3356231fa795f35dd56a492681b
SumaVarshitha6 <Sumavarshitha216@gmail.com>
2023-05-31 13:06:39 +0530

    hooks

--- mypractice/src/App.js
+++ mypractice/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useMemo} from 'react';
 
 function App() {
+  var [x, setX] = React.useState();
+  var [a, setA] = React.useState(6);
+  var [b, setB] = React.useState(6);
+  var add=useMemo(()=>{
+    return a+b;
+  },[a,b])
+  var title=useMemo(()=>{
+    return x;
+  },[x])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App.....</h1>x :{' '}
+      <input type="text" onChange={(e)=>{setX(e.target.value)}}/><br></br> <br></br>a:{' '}
+      <input type="text" onChange={(e)=>{setA(+e.target.value)}}/><br></br>b:{' '}
+      <input type="text" onChange={(e)=>{setB(+e.target.value)}}/>
+      <h3>{title}</h3>
+      <h1>{add}</h1>
     </div>
   );
 }
