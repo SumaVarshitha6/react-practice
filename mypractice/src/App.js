369ed428429386723558a31a70d802200947e522
SumaVarshitha6 <Sumavarshitha216@gmail.com>
2023-06-08 11:33:43 +0530

    crud formik

--- mypractice/src/App.js
+++ mypractice/src/App.js
@@ -1,12 +1,30 @@
-
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 function App() {
- 
+  var Userform = useFormik({
+    initialValues: {
+      name:'',
+      age:'',
+    },
+    validationSchema: Yup.object({
+        name: Yup.string()
+          .max(15, 'Must be 15 characters or less')
+          .required('Required'),
+        age: Yup.number()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required'),
+      }),
+    onSubmit: (values)=>{
+      console.log(values);
+    }
+  }) 
   return (
-    <div className="App border border-2 border-danger ">
-         <h1>My App</h1>
-         
-    </div>
+    <form onSubmit={Userform.handleSubmit} className='border border-2 border-info m-5 p-3'>
+    <h1>Useformik</h1>
+    <input type='text' {...Userform.getFieldProps('name')} /><br></br>
+    <input type='number' {...Userform.getFieldProps('age')}/><br></br>
+    <button type='button'>Save</button>
+ </form>
   );
 }
-
 export default App;
